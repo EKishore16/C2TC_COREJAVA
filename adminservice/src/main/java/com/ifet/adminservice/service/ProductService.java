cae5a23bb22eef1a69cb29e369c657eddaa32d1e
kishore <kishore1610@example.com>
2025-02-05 17:05:13 +0530

    AdminService first Commit

---
+++ adminservice/src/main/java/com/ifet/adminservice/service/ProductService.java
@@ -0,0 +1,26 @@
+package com.ifet.adminservice.service;
+import com.ifet.adminservice.model.Product;
+import com.ifet.adminservice.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product saveProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
+    }
+}
