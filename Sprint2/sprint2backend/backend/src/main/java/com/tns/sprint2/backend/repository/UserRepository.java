09af756d2143b31f6c7177ae26306d75d952970d
unknown <kishore1610@example.com>
2025-03-09 12:43:44 +0530

    Added all Sprint2 project files

--- Sprint2/sprint2backend/backend/src/main/java/com/tns/sprint2/backend/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tns.sprint2.backend.repository;
-
-import com.tns.sprint2.backend.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
